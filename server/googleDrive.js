c6d51969e878e41cab8d46386a3d406ca2820134
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-05-08 14:42:01 +0100

    Add google drive modification implementation

--- server/googleDrive.js
+++ server/googleDrive.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  'http://localhost:5000/api/auth/callback'
 );
 
 // Générer l'URL d'authentification
