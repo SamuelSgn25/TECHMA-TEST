d8565bf033878d191bc05b26199901686d63f8cf
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-05-08 11:01:43 +0100

    Add database configuration

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// Configuration segmentée
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.on('connect', () => {
+  console.log('PostgreSQL: Connecté avec succès');
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
